6ac990146e3f419412dff9c8af017fea717cb78f
Bryan Flores <elton.flores.a@uni.pe>
2023-01-07 07:12:30 -0500

    feat: add schema validator

--- src/network/routes/email.js
+++ src/network/routes/email.js
@@ -1,21 +1,25 @@
 import { Router } from 'express'
 import { sendEmail } from '../../controllers/index.js'
 import { response } from './../index.js'
+import { validateNotification } from './middlewares/index.js'
 const Email = Router()
 
-Email.route('/email/sendEmail').post(async (req, res, next) => {
-  const notification = req.body
-  try {
-    await sendEmail(notification)
-    response({
-      error: false,
-      message: 'Email sent',
-      res,
-      status: 200
-    })
-  } catch (err) {
-    next(err)
+Email.route('/email/sendEmail').post(
+  validateNotification,
+  async (req, res, next) => {
+    const notification = req.body
+    try {
+      await sendEmail(notification)
+      response({
+        error: false,
+        message: 'Email sent',
+        res,
+        status: 200
+      })
+    } catch (err) {
+      next(err)
+    }
   }
-})
+)
 
 export { Email }
--- src/network/routes/middlewares/index.js
+++ src/network/routes/middlewares/index.js
@@ -1,3 +1,17 @@
+import Joi from 'joi'
+import httpErrors from 'http-errors'
+import { notification as notificationSchema } from '../../../schemas/index.js'
+
+const validateNotification = async (req, res, next) => {
+  try {
+    await notificationSchema.validateAsync(req.body)
+  } catch (e) {
+    if (e instanceof Joi.ValidationError)
+      next(new httpErrors.UnprocessableEntity(e.message))
+    next(e)
+  }
+}
+
 const validateApiKey = (req, res, next) => {
   const apiKey = req.headers['api-key']
   if (!apiKey || apiKey !== process.env.API_KEY)
@@ -5,4 +19,4 @@ const validateApiKey = (req, res, next) => {
   else next()
 }
 
-export { validateApiKey }
+export { validateApiKey, validateNotification }
