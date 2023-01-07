1cb92433ee55e5281a6782eef28c3bc03919fb98
Bryan Flores <elton.flores.a@uni.pe>
2023-01-07 01:20:12 -0500

    delete unnecessary variable

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -11,19 +11,15 @@ const sendEmail = async notification => {
     messages[id].time = time
   } else messages[id] = { email, time, message }
 
-  let reponseSendEmail = null
-
   messages[id].timeout = setTimeout(async () => {
     try {
-      reponseSendEmail = await sendEmailNodemailer(email, messages[id].message)
+      await sendEmailNodemailer(email, messages[id].message)
       delete messages[id]
     } catch (err) {
       return errorHandling(err, 'Error sending email')
     }
   }, time * 1000)
   console.log([email, messages[id].message])
-
-  if (reponseSendEmail !== null) return reponseSendEmail
 }
 
 const sendEmailNodemailer = async (email, message) => {
--- src/controllers/utils/index.js
+++ src/controllers/utils/index.js
@@ -1,9 +1,8 @@
 import httpErrors from 'http-errors'
-import { GE, EFE, MFE } from './messages/index.js'
 const errorHandling = (error, message) => {
   if (error instanceof httpErrors.HttpError) throw error
 
   throw new httpErrors.InternalServerError(message ?? error.message)
 }
 
-export { errorHandling, GE, EFE, MFE }
+export { errorHandling }
